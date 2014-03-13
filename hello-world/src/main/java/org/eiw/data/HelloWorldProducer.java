1ac8bf3d672d705dbe92b18e9362f37bcae9be7a
Yan Langlois <langlois_yan@yahoo.fr>
2014-03-13 23:19:23 +0100

    ajout commentaires

--- hello-world/src/main/java/org/eiw/data/HelloWorldProducer.java
+++ hello-world/src/main/java/org/eiw/data/HelloWorldProducer.java
@@ -4,17 +4,27 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
-
+/**
+ * Le producer es dans le scope Request,
+ * il sera recréé à chaque nouvelle requete HTTP.
+ */
 @RequestScoped
 public class HelloWorldProducer {
 	private String message;
-	
+	/**
+    	 * La méthode qui produit l'objet de données vis à vis de la page XHTML.
+    	 */
 	@Named
 	@Produces
 	public String getMessage() {
 		return message;
 	}
 	
+	
+	/**
+     	 * La méthode qui initialise l'objet,
+     	 * à chaque nouvelle requête HTTP.
+     	 */
 	@PostConstruct
 	public void initMessage() {
 		System.out.println("s<dfdsf");
